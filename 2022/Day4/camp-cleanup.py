be941a75150573419f0b6d54aeb50c338b169d40
theredditbandit <85390033+theredditbandit@users.noreply.github.com>
2023-07-24 22:38:43 +0530

    Add day4 part 2 sol

--- 2022/Day4/camp-cleanup.py
+++ 2022/Day4/camp-cleanup.py
@@ -3,15 +3,19 @@ def main():
         contents = f.readlines()
 
     total_overlaps = 0
+    partial_overlaps = 0
     for i in contents:
         elf1 , elf2 = i.strip().split(",")
         
-        if isOverlapping(elf1,elf2):
+        if isFullyOverlapping(elf1,elf2):
             total_overlaps += 1
+        if isPartiallyOverlapping(elf1,elf2):
+            partial_overlaps += 1
             
     print(f"{total_overlaps=}")
+    print(f"{partial_overlaps=}")
 
-def isOverlapping(elf1,elf2) -> bool:
+def isFullyOverlapping(elf1,elf2) -> bool:
     e1a , e1b = map(int,elf1.split("-"))
     e2a , e2b = map(int,elf2.split("-"))
     
@@ -21,6 +25,17 @@ def isOverlapping(elf1,elf2) -> bool:
     if e1set.issubset(e2set) or e2set.issubset(e1set):
         return True
     return False
+
+def isPartiallyOverlapping(elf1,elf2) -> bool:
+    e1a , e1b = map(int,elf1.split("-"))
+    e2a , e2b = map(int,elf2.split("-"))
+    
+    e1set = set(range(e1a,e1b+1))
+    e2set = set(range(e2a,e2b+1))
+    
+    if e1set.intersection(e2set):
+        return True
+
 
 
 
