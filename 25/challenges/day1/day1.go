cb1a92b0aadd0a3d527cdea4699faa1d3f176cf7
aryan <85390033+theredditbandit@users.noreply.github.com>
2025-12-02 11:46:55 +0530

    solve day1

--- 25/challenges/day1/day1.go
+++ 25/challenges/day1/day1.go
@@ -25,40 +25,30 @@ func Sol(mode string) {
 			log.Fatal(err)
 		}
 
-		crossed, newPos, finalZeros := getZerosCrossed(currPos, distance, direction)
-		zerosCrossed += crossed
-		zeroCount += finalZeros
-		currPos = newPos
+		switch direction {
+		case "L":
+			dist := distance % 100
+			zerosCrossed += (distance - dist) / 100
+			for _ = range dist {
+				currPos = ((currPos-1)%100 + 100) % 100
+				if currPos == 0 {
+					zerosCrossed++
+				}
+			}
+
+		case "R":
+			dist := distance % 100
+			zerosCrossed += (distance - dist) / 100
+			for _ = range dist {
+				currPos = (currPos + 1) % 100
+				if currPos == 0 {
+					zerosCrossed++
+				}
+			}
+		}
+
 		log.Debugf("op (%s) -> pos (%d) . zeros  crossed (%d)", line, currPos, zerosCrossed)
 	}
 	log.Infof("Password to open door is %d", zeroCount)
 	log.Infof("Total zeros crossed is %d", zerosCrossed)
-}
-
-func getZerosCrossed(currPos int, distance int, direction string) (zerosCrossed int, newPos int, finalZeros int) {
-	switch direction {
-	case "L":
-		newPos = currPos - distance
-
-		distRemainder := distance % 100
-		zerosCrossed = int(distance / 100)
-		if newPos < 0 {
-			newPos = newPos%100 + 100
-		}
-
-		if distRemainder >= currPos && currPos != 0 {
-			zerosCrossed++
-		}
-	case "R":
-		newPos = currPos + distance
-		zerosCrossed = int(newPos / 100)
-
-		newPos = newPos % 100
-	}
-
-	if newPos%100 == 0 { // part 1
-		finalZeros++
-	}
-
-	return zerosCrossed, newPos, finalZeros
 }
