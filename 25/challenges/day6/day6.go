7f295f3c2f9778a2a1a79c41962b185ee4058fca
aryan <85390033+theredditbandit@users.noreply.github.com>
2025-12-06 18:35:35 +0530

    solve d6p2

--- 25/challenges/day6/day6.go
+++ 25/challenges/day6/day6.go
@@ -2,6 +2,8 @@ package day6
 
 import (
 	"25/utils"
+	"errors"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -12,22 +14,67 @@ import (
 	"github.com/charmbracelet/log"
 )
 
+func transformInput(input [][]string) [][]string {
+	transformed := []string{}
+	for j := range len(input[0][0]) {
+		num := []byte{}
+		for i := range len(input) - 1 {
+			r := input[i][0][j]
+			num = append(num, r)
+		}
+		transformed = append(transformed, string(num))
+	}
+	tempNum := []string{}
+	nums := [][]string{}
+	for i := range len(transformed) - 1 {
+		ele := strings.Trim(transformed[i], " ")
+		tempNum = append(tempNum, ele)
+		if transformed[i+1] == "    " { // TODO : make this 4 inputs for final and 3 for test
+			nums = append(nums, tempNum)
+			tempNum = []string{}
+		}
+	}
+	nums = append(nums, tempNum)
+	nums[len(nums)-1] = append(nums[len(nums)-1], strings.Trim(transformed[len(transformed)-1], " "))
+	return nums
+}
+
+func solve(operators string, inputs [][]string) int {
+	var sol int
+	op := strings.Fields(operators)
+	for oIdx, o := range op {
+		nums := inputs[oIdx]
+		log.Debug(nums)
+		var exp string
+		for _, num := range nums {
+			exp += strings.Trim(num, " ") + o
+		}
+		exp = strings.Trim(exp, o)
+		astExp, _ := parser.ParseExpr(exp)
+		res := Eval(astExp)
+		log.Debugf("%s = %d", exp, res)
+		sol += res
+	}
+	return sol
+}
+
 func Sol(mode string) {
 	data, file := utils.GetInput(6, mode)
 	defer file.Close()
-
-	problem := [][]string{}
+	part1Problem := [][]string{}
+	part2Problem := [][]string{}
 	for data.Scan() {
 		line := data.Text()
 		nums := strings.Fields(line)
-		problem = append(problem, nums)
+		nums2 := strings.Split(line, "\n")
+		part1Problem = append(part1Problem, nums)
+		part2Problem = append(part2Problem, nums2)
 	}
 
 	grandTotal := 0
-
-	for i, op := range problem[len(problem)-1] {
+	for i, op := range part1Problem[len(part1Problem)-1] {
 		var exp string
-		for _, numbers := range problem[:len(problem)-1] {
+		for _, numbers := range part1Problem[:len(part1Problem)-1] {
 			exp += numbers[i] + " " + op + " "
 		}
 		exp = strings.TrimRight(exp, " "+op+" ")
@@ -35,14 +82,15 @@ func Sol(mode string) {
 		if err != nil {
 			log.Fatalf("Could not parse expression %s", exp)
 		}
-
 		res := Eval(astExpr)
 		grandTotal += res
-
 		log.Debugf("%s = %d", exp, res)
 	}
-
 	log.Infof("Solution to part 1 is %d", grandTotal)
+	transformedInput := transformInput(part2Problem)
+	log.Debug(transformedInput)
+	sol := solve(part2Problem[len(part2Problem)-1][0], transformedInput)
+	log.Infof("Solution to p2 is %d", sol)
 }
 
 func Eval(exp ast.Expr) int {
@@ -56,14 +104,12 @@ func Eval(exp ast.Expr) int {
 			return i
 		}
 	}
-
 	return 0
 }
 
 func EvalBinaryExpr(exp *ast.BinaryExpr) int {
 	left := Eval(exp.X)
 	right := Eval(exp.Y)
-
 	switch exp.Op {
 	case token.ADD:
 		return left + right
@@ -74,6 +120,5 @@ func EvalBinaryExpr(exp *ast.BinaryExpr) int {
 	case token.QUO:
 		return left / right
 	}
-
 	return 0
 }
