bc456f09cad1f7076d91eb5a7813216f5ccc9079
aryan <85390033+theredditbandit@users.noreply.github.com>
2025-12-08 20:31:30 +0530

    solve d7p2

--- 25/challenges/day7/day7.go
+++ 25/challenges/day7/day7.go
@@ -3,46 +3,68 @@ package day7
 import (
 	"25/utils"
 	"slices"
+	"strconv"
 	"strings"
 
 	"github.com/charmbracelet/log"
 )
 
-func beamSplitter(grid [][]string, rowIdx, splits int) ([][]string, int, int) {
+func beamSplitter(grid [][]string, rowIdx, timelines int) ([][]string, int, int) {
 	if rowIdx == len(grid) {
-		return grid, rowIdx, splits
+		return grid, rowIdx, timelines
 	}
 	if rowIdx == 0 { // start condition
 		log.Debug(grid[rowIdx])
 		Sidx := slices.Index(grid[rowIdx], "S")
 		rowIdx++
-		grid[rowIdx][Sidx] = "|"
+		grid[rowIdx][Sidx] = "1"
 		log.Debug(grid[rowIdx])
 		rowIdx++
 	} else if slices.Contains(grid[rowIdx], "^") {
 		for i, ele := range grid[rowIdx] {
 			prev := grid[rowIdx-1][i]
-			if ele == "^" && prev == "|" {
-				grid[rowIdx][i-1] = "|"
-				grid[rowIdx][i+1] = "|"
-				splits++
-			} else if prev == "|" {
-				grid[rowIdx][i] = "|"
+			prevNum, err := strconv.Atoi(prev)
+			if ele == "^" && err == nil {
+				currL := grid[rowIdx][i-1]
+				currR := grid[rowIdx][i+1]
+
+				lNum, lErr := strconv.Atoi(currL)
+				rNum, rErr := strconv.Atoi(currR)
+
+				if lErr == nil { // i.e. there is already a num there
+					grid[rowIdx][i-1] = strconv.Itoa(lNum + prevNum)
+				} else {
+					grid[rowIdx][i-1] = prev
+				}
+
+				if rErr == nil {
+					grid[rowIdx][i+1] = strconv.Itoa(rNum + prevNum)
+				} else {
+					grid[rowIdx][i+1] = prev
+				}
+				timelines++
+			} else if prev != "." && prev != "^" {
+				curr, err := strconv.Atoi(grid[rowIdx][i])
+				if err == nil {
+					grid[rowIdx][i] = strconv.Itoa(curr + prevNum)
+				} else {
+					grid[rowIdx][i] = prev
+				}
 			}
 		}
 		log.Debug(grid[rowIdx])
 		rowIdx++
 	} else {
 		for i := range grid[rowIdx] {
-			ele := grid[rowIdx-1][i]
-			if ele == "|" {
-				grid[rowIdx][i] = "|"
+			prev := grid[rowIdx-1][i]
+			if prev != "." && prev != "^" {
+				grid[rowIdx][i] = prev
 			}
 		}
 		log.Debug(grid[rowIdx])
 		rowIdx++
 	}
-	return beamSplitter(grid, rowIdx, splits)
+	return beamSplitter(grid, rowIdx, timelines)
 }
 
 func Sol(mode string) {
@@ -56,8 +78,16 @@ func Sol(mode string) {
 		grid = append(grid, row)
 	}
 
-	_, _, splits := beamSplitter(grid, 0, 0)
+	mutatedGrid, _, splits := beamSplitter(grid, 0, 0)
+	log.Infof("%d splits created", splits)
 
-	log.Infof("Beam was split %d times", splits)
+	timelines := 0
+	for _, ele := range mutatedGrid[len(grid)-1] {
+		num, err := strconv.Atoi(ele)
+		if err == nil {
+			timelines += num
+		}
+	}
+	log.Infof("%d timelines created", timelines)
 
 }
